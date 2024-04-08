91caff190ac4a0d6ddc324dfabb3979f06294981
pyunghun <564564q@naver.com>
2024-04-08 14:57:13 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/section02/annotation/LifeCycleTestServlet.java
@@ -0,0 +1,35 @@
+package com.ohgiraffers.section02.annotation;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+@WebServlet(value = "/annotation-lifecycle")
+
+public class LifeCycleTestServlet extends HttpServlet {
+
+    private int initCount = 1;
+    private int serviceCount = 1;
+    private int destroyCount = 1;
+    public LifeCycleTestServlet() {}
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("어노테이션 service" + serviceCount++);
+
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("어노테이션 destroy" + destroyCount++);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("어노테이션 init() " + initCount++);
+    }
+}
